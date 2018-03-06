23a35385bfb03e91344230105002aa530d719a8d
chrisME1320 <chrisme1320@gmail.com>
2018-03-06 16:59:49 -0600

    La escena de la luz y un trigger que apaga una luz que se le pasa y prende la otra luz

---
+++ Assets/src/TurnOnSpotlight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnSpotlight : MonoBehaviour {
+	public GameObject spotlight;
+	public GameObject doorSpotLight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other){
+		Light doorLight = doorSpotLight.GetComponent<Light> ();
+		Light light = spotlight.GetComponent<Light> ();
+		if (other.gameObject.tag=="MainCamera") {
+			light.enabled = false;
+			doorLight.enabled = true;
+
+		}
+
+	}
+}
