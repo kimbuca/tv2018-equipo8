2e816c1197c09f496945759eed3be10bd3967916
M8858X <a01228858@itesm.mx>
2018-03-07 18:43:34 -0600

    Efecto detransicion entre escenas FadeIn/FadeOut

---
+++ Assets/transition/cambioEscena.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class cambioEscena : MonoBehaviour {
+ 
+    public Animator anim;
+    public Image Img;
+    public string scene;
+
+    
+    IEnumerator Fade()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => Img.color.a == 1);
+        SceneManager.LoadScene(scene);
+    }
+ 
+    private void OnCollisionEnter(Collision collision)
+    {
+        StartCoroutine(Fade());
+       
+    }
+    
+}
