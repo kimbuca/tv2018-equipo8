480a3cea8a755babb8da6cafb013f7343a913db7
Kimberly Luna <kimberly@Kimberlys-MacBook-Air.local>
2018-03-08 23:28:49 -0600

    changes to unmount

---
+++ Assets/unmountTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unmountTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	IEnumerator destroyAll() {
+		
+		var destructables = Object.FindObjectsOfType<unmount> ();
+
+		foreach (unmount destructable in destructables) {
+			destructable.destroyFloor ();
+			yield return new WaitForSeconds (1f);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyUp("space")) {	
+			StartCoroutine ("destroyAll");
+		}
+	}
+}
